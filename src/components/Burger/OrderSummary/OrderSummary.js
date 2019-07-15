86acac8f24ffa00091b6114aa4765a1276bd0325
tylerstruntz <tylerstruntz@gmail.com>
2019-07-15 14:42:45 -0400

    moved some folders around

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Auxillary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientsUsed = Object.keys(props.ingredient).map(igKey => {
-        return( 
-            <li key={igKey}>
-                <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredient[igKey]}
-            </li>
+class OrderSummary extends Component {
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[OrderSummary] WillUpdate');
+    }
+
+
+    render() {
+        const ingredientsUsed = Object.keys(this.props.ingredient).map(igKey => {
+            return (
+                <li key={ igKey }>
+                    <span style={ { textTransform: 'capitalize' } }>{ igKey }</span>: { this.props.ingredient[igKey] }
+                </li>
+            )
+        });
+        return (
+            <Aux>
+                <h3>Your Order</h3><br />
+                <p>Ingredients</p>
+                <ul>
+                    { ingredientsUsed }
+                </ul>
+                <p>Your order costs: $<strong>{ this.props.orderPrice.toFixed(2) }</strong></p>
+                <p>Continue to checkout?</p>
+                <Button
+                    btnType='Danger'
+                    clicked={ this.props.purchaseCanceled }>Cancel</Button>
+                <Button
+                    btnType='Success'
+                    clicked={ this.props.purchaseContinues }>Continue</Button>
+            </Aux>
         )
-    });
-    
-    return(
-        <Aux>
-            <h3>Your Order</h3><br />
-            <p>Ingredients</p>
-            <ul>
-                {ingredientsUsed}
-            </ul>
-            <p>Your order costs: $<strong>{props.orderPrice.toFixed(2)}</strong></p>
-            <p>Continue to checkout?</p>
-            <Button 
-                btnType='Danger'
-                clicked={props.purchaseCanceled}>Cancel</Button>
-            <Button 
-                btnType='Success'
-                clicked={props.purchaseContinues}>Continue</Button>
-        </Aux>
-    )
+    }
+
 };
 
-export default orderSummary;
+export default OrderSummary;
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import Aux from '../../hoc/Auxillary';
 import classes from './Layout.css';
-import Toolbar from '../Navigation/Toolbar/Toolbar';
-import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer'
 
 class Layout extends Component {
     state = {
-        sideDrawerVisible: true,
+        sideDrawerVisible: false,
     }
 
     //set the new state depending on the old state
